3c7021096fdf72a943c35598894c0237e048a836
highonskooma <joaoguireis99@gmail.com>
2024-03-15 16:11:28 +0000

    added nvim themes +  disabled picom

--- .config/nvim/lua/blackgaze/plugins/colorscheme.lua
+++ .config/nvim/lua/blackgaze/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
+local colorschemes = {
+	['nightfly'] = 'bluz71/vim-nightfly-colors',
+	['dracula'] = 'Mofiqul/dracula.nvim',
+	['tokyonight'] = 'folke/tokyonight.nvim', -- night, storm, day, moon
+	['catppuccin'] = 'catppuccin/nvim', -- latte, frappe, macchiato, mocha
+	['gruvbox'] = 'ellisonleao/gruvbox.nvim'
+}
+
+local selectedColorscheme = 'catppuccin' --change here
+local style = ''
+
 return {
-	-- "bluz71/vim-nightfly-colors",
-	"Mofiqul/dracula.nvim",
+	colorschemes[selectedColorscheme],
+	lazy = false,
 	priority = 1000,
 	config = function()
 		-- load colorscheme here
-		vim.cmd([[colorscheme dracula]])
+		local cmd = 'colorscheme ' .. selectedColorscheme .. style
+		vim.cmd(cmd)
 	end,
 }
