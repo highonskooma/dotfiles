27ad53f1361ac232b13ee386d507cc2d6771860d
highonskooma <joaoguireis99@gmail.com>
2024-10-07 21:48:38 +0100

    added diagflow and simplified dingllm

--- .config/nvim/lua/blackgaze/init.lua
+++ .config/nvim/lua/blackgaze/init.lua
@@ -3,8 +3,11 @@ require("blackgaze.remap")
 vim.o.clipboard = 'unnamedplus'
 vim.o.number = true
 vim.o.signcolumn = 'yes'
+vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.autoindent = true
 vim.o.updatetime = 300
 vim.o.mouse = 'a'
 vim.updatetime = 100
