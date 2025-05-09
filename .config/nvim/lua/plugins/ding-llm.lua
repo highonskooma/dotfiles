15119ce795dbf14544420420c35e972ec262fba3
highonskooma <joaoguireis99@gmail.com>
2025-05-09 18:53:35 +0100

    changes may 2025

--- .config/nvim/lua/plugins/ding-llm.lua
+++ .config/nvim/lua/plugins/ding-llm.lua
@@ -1,85 +1,85 @@
 return {
-    'highonskooma/dingllm.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local system_prompt =
-        'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
-      local helpful_prompt = 'You are a helpful assistant. What I have sent are my notes so far.'
-      local dingllm = require 'dingllm'
+	"highonskooma/dingllm.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local system_prompt =
+			"You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks"
+		local helpful_prompt = "You are a helpful assistant. What I have sent are my notes so far."
+		local dingllm = require("dingllm")
 
-      local function make_ollama_spec_curl_args(opts, prompt)
-          local url = opts.url
-          local data = {
-              model = "dolphin-llama3:latest",
-              messages = { { role = 'system', content = system_prompt }, { role = 'user', content = prompt } },
-              stream = true,
-          }
-          local args = { '-d', vim.fn.json_encode(data) }
-          table.insert(args, url)
-          return args
-      end
+		local function make_ollama_spec_curl_args(opts, prompt)
+			local url = opts.url
+			local data = {
+				model = "dolphin-llama3:latest",
+				messages = { { role = "system", content = system_prompt }, { role = "user", content = prompt } },
+				stream = true,
+			}
+			local args = { "-d", vim.fn.json_encode(data) }
+			table.insert(args, url)
+			return args
+		end
 
-      local function ollama_replace()
-        dingllm.invoke_llm_and_stream_into_editor({
-          url = 'http://localhost:11434/api/chat',
-          model = 'llama3.1:8b',
-          system = system_prompt,
-        }, make_ollama_spec_curl_args, dingllm.handle_ollama_spec_data)
-      end
+		local function ollama_replace()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "http://localhost:11434/api/chat",
+				model = "llama3.1:8b",
+				system = system_prompt,
+			}, make_ollama_spec_curl_args, dingllm.handle_ollama_spec_data)
+		end
 
-      local function ollama_help()
-          dingllm.invoke_llm_and_stream_into_editor({
-              url = 'http://localhost:11434/api/chat',
-              model = 'llama3.1:8b',
-              system = helpful_prompt,
-          }, make_ollama_spec_curl_args, dingllm.handle_ollama_spec_data)
-      end
+		local function ollama_help()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "http://localhost:11434/api/chat",
+				model = "llama3.1:8b",
+				system = helpful_prompt,
+			}, make_ollama_spec_curl_args, dingllm.handle_ollama_spec_data)
+		end
 
-      local function groq_replace()
-        dingllm.invoke_llm_and_stream_into_editor({
-          url = 'https://api.groq.com/openai/v1/chat/completions',
-          model = 'llama-3.1-70b-versatile',
-          api_key_name = 'GROQ_API_KEY',
-          system_prompt = system_prompt,
-          replace = true,
-        }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      end
+		local function groq_replace()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "https://api.groq.com/openai/v1/chat/completions",
+				model = "llama-3.1-70b-versatile",
+				api_key_name = "GROQ_API_KEY",
+				system_prompt = system_prompt,
+				replace = true,
+			}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
+		end
 
-      local function groq_help()
-        dingllm.invoke_llm_and_stream_into_editor({
-          url = 'https://api.groq.com/openai/v1/chat/completions',
-          model = 'llama-3.1-70b-versatile',
-          api_key_name = 'GROQ_API_KEY',
-          system_prompt = helpful_prompt,
-          replace = false,
-        }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      end
+		local function groq_help()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "https://api.groq.com/openai/v1/chat/completions",
+				model = "llama-3.1-70b-versatile",
+				api_key_name = "GROQ_API_KEY",
+				system_prompt = helpful_prompt,
+				replace = false,
+			}, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
+		end
 
-      local function anthropic_help()
-        dingllm.invoke_llm_and_stream_into_editor({
-          url = 'https://api.anthropic.com/v1/messages',
-          model = 'claude-3-5-sonnet-20240620',
-          api_key_name = 'ANTHROPIC_API_KEY',
-          system_prompt = helpful_prompt,
-          replace = false,
-        }, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-      end
+		local function anthropic_help()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "https://api.anthropic.com/v1/messages",
+				model = "claude-3-5-sonnet-20240620",
+				api_key_name = "ANTHROPIC_API_KEY",
+				system_prompt = helpful_prompt,
+				replace = false,
+			}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
+		end
 
-      local function anthropic_replace()
-        dingllm.invoke_llm_and_stream_into_editor({
-          url = 'https://api.anthropic.com/v1/messages',
-          model = 'claude-3-5-sonnet-20240620',
-          api_key_name = 'ANTHROPIC_API_KEY',
-          system_prompt = system_prompt,
-          replace = true,
-        }, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-      end
+		local function anthropic_replace()
+			dingllm.invoke_llm_and_stream_into_editor({
+				url = "https://api.anthropic.com/v1/messages",
+				model = "claude-3-5-sonnet-20240620",
+				api_key_name = "ANTHROPIC_API_KEY",
+				system_prompt = system_prompt,
+				replace = true,
+			}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
+		end
 
-		  vim.keymap.set({ 'n', 'v' }, '<leader>z', ollama_replace, { desc = 'ollama base' }) -- explain the codevim.keymap.set
-			vim.keymap.set({ 'n', 'v' }, '<leader>T', ollama_help, { desc = 'ollama help' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>k', groq_replace, { desc = 'llm groq' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>K', groq_help, { desc = 'llm groq_help' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>I', anthropic_help, { desc = 'llm anthropic_help' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>i', anthropic_replace, { desc = 'llm anthropic' })
-    end,
-  }
+		vim.keymap.set({ "n", "v" }, "<leader>z", ollama_replace, { desc = "ollama base" }) -- explain the codevim.keymap.set
+		vim.keymap.set({ "n", "v" }, "<leader>T", ollama_help, { desc = "ollama help" })
+		vim.keymap.set({ "n", "v" }, "<leader>k", groq_replace, { desc = "llm groq" })
+		vim.keymap.set({ "n", "v" }, "<leader>K", groq_help, { desc = "llm groq_help" })
+		vim.keymap.set({ "n", "v" }, "<leader>I", anthropic_help, { desc = "llm anthropic_help" })
+		vim.keymap.set({ "n", "v" }, "<leader>i", anthropic_replace, { desc = "llm anthropic" })
+	end,
+}
--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,51 +1,59 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require('gitsigns').setup{
-            on_attach = function(bufnr)
-                local gitsigns = require('gitsigns')
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
 
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-                -- Navigation
-                map('n', ']c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({']c', bang = true})
-                    else
-                        gitsigns.nav_hunk('next')
-                    end
-                end)
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
 
-                map('n', '[c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({'[c', bang = true})
-                    else
-                        gitsigns.nav_hunk('prev')
-                    end
-                end)
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
 
-                -- Actions
-                map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Stage hunk" })
-                map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Reset hunk" })
-                map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-                map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-                map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Stage buffer" })
-                map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Undo Stage bugger" })
-                map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Reset buffer" })
-                map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Preview hunk" })
-                map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
-                map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
-                map('n', '<leader>hd', gitsigns.diffthis, { desc = "Diff this" })
-                map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-                map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+				-- Actions
+				map("n", "<leader>ghs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+				map("n", "<leader>ghr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+				map("v", "<leader>ghs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Stage hunk" })
+				map("v", "<leader>ghr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Reset hunk" })
+				map("n", "<leader>ghS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+				map("n", "<leader>ghu", gitsigns.undo_stage_hunk, { desc = "Undo Stage bugger" })
+				map("n", "<leader>ghR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+				map("n", "<leader>ghp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+				map("n", "<leader>ghb", function()
+					gitsigns.blame_line({ full = true })
+				end, { desc = "Blame line" })
+				map("n", "<leader>ght", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+				map("n", "<leader>ghd", gitsigns.diffthis, { desc = "Diff this" })
+				map("n", "<leader>ghD", function()
+					gitsigns.diffthis("~")
+				end, { desc = "Diff ~" })
+				map("n", "<leader>ghh", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
 
-                -- Text object
-                map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-            end
-        }
-    end
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+			end,
+		})
+	end,
 }
